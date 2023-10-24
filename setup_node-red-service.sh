3b74b052714a10bde8d92bd9964986152030e471
FlorianHandke <34957612+FlorianHandke@users.noreply.github.com>
2023-10-24 15:13:32 +0200

    initial

---
+++ setup_node-red-service.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Check if the script is running as root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# Create the Node-RED service unit file
+echo "[Unit]
+Description=Node-Red
+
+[Service]
+User=admin
+ExecStart=node-red
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target" | sudo tee /etc/systemd/system/node-red.service
+
+# Enable and start the Node-RED service
+systemctl enable node-red.service
+systemctl start node-red.service
+
+# Check the status of the Node-RED service
+systemctl status node-red.service
+
+echo "Node-RED has been configured as a systemd service."
+
+# Provide instructions for accessing Node-RED
+echo "You can access Node-RED by opening a web browser and navigating to:"
+echo "http://localhost:1880 (if you're on the Raspberry Pi itself)"
+echo "or"
+echo "http://<your_pi_IP_address>:1880 (from another device on the same network)"
